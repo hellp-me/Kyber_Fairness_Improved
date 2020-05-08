bdc0aeeb29bf93b5b13b59debc77c4553cde6ccd
Hyeonsoo Kim <alcatraz@pusan.ac.kr>
2020-05-08 23:19:02 +0900

    [#10] correct blk-cgroup files

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -6339,7 +6339,7 @@ static int __init bfq_init(void)
 	int ret;
 
 #ifdef CONFIG_BFQ_GROUP_IOSCHED
-	ret = blkcg_policy_register(&blkcg_policy_bfq, &iosched_bfq_mq);
+	ret = blkcg_policy_register(&blkcg_policy_bfq);
 	if (ret)
 		return ret;
 #endif
