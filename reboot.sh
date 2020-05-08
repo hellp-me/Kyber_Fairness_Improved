51be9adb0ded07a040a600135a2daa46c8f3d379
Hyeonsoo Kim <alcatraz@pusan.ac.kr>
2020-05-08 23:20:56 +0900

    [#11] make script is correted

--- reboot.sh
+++ reboot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 make -j3 && \
-rm -rf /boot/vmlinuz-5.2.0+ && rm -rf /boot/initrd.img-5.2.0+ && \
-make modules_install -j3 && make install -j3 && reboot
+sudo rm -rf /boot/vmlinuz-5.2.0+ && sudo rm -rf /boot/initrd.img-5.2.0+ && \
+sudo make modules_install -j3 && sudo make install -j3 && reboot
